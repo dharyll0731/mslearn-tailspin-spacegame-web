991eae2ec060c7d519c31889eaeae03b8828d851
gitName <a@b.c>
2025-07-23 09:12:18 +0800

    Return correct number of items

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -50,7 +50,7 @@ namespace TailSpin.SpaceGame.Web
         /// <param name="orderDescendingPredicate">Predicate that specifies how to sort the results in descending order.</param>
         /// <param name="page">The 1-based page of results to return.</param>
         /// <param name="pageSize">The number of items on a page.</param>
-        public Task<IEnumerable<T>> GetItemsAsync(
+       public Task<IEnumerable<T>> GetItemsAsync(
             Func<T, bool> queryPredicate,
             Func<T, int> orderDescendingPredicate,
             int page = 1, int pageSize = 10
@@ -60,7 +60,7 @@ namespace TailSpin.SpaceGame.Web
                 .Where(queryPredicate) // filter
                 .OrderByDescending(orderDescendingPredicate) // sort
                 .Skip(page * pageSize) // find page
-                .Take(pageSize - 1); // take items
+                .Take(pageSize); // take items
 
             return Task<IEnumerable<T>>.FromResult(result);
         }
